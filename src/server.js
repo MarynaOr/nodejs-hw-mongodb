86637617e56425f1c940337cc96909b7db833707
Maryna <89296066105marina@gmal.com>
2025-06-21 14:31:46 +0200

    fix-status-fav+db

--- src/server.js
+++ src/server.js
@@ -26,29 +26,32 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
-      data: contacts,
+      status: 200,
       message: 'Successfully found contacts!',
+      data: contacts,
     });
   });
-  app.get('/contacts/:contactId', async (req, res, _next) => {
+
+  app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactId(contactId);
 
     if (!contact) {
       return res.status(404).json({
+        status: 404,
         message: 'Contact not found',
       });
     }
 
     res.status(200).json({
-      data: contact,
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
-    return;
   });
-
   app.use((req, res) => {
-    res.status(404).json({
+    res.json({
+      status: 404,
       message: 'Not found',
     });
   });
@@ -56,7 +59,10 @@ export const setupServer = () => {
   app.use((err, req, res, _next) => {
     console.log(err.stack);
 
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.json({
+      status: 500,
+      message: 'Internal Server Error',
+    });
   });
 
   app.listen(PORT, () => {
