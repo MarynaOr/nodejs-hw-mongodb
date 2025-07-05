5d8693b9f945ffd955d203d5b9791e613c53864b
Maryna <89296066105marina@gmal.com>
2025-07-05 13:46:48 +0200

    task-2

---
+++ src/validation/contacts.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+export const createValidationContact = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phone: Joi.string().min(3).max(20).required(),
+});
+
+export const updateValidationContact = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phone: Joi.string().min(3).max(20),
+}).or('name', 'phone');
