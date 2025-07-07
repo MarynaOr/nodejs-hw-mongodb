db1bc43c9654bccda36d5914a038ee92950ad556
Maryna <89296066105marina@gmal.com>
2025-07-07 12:40:54 +0200

    fixing-sort-patch-post

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,9 +3,16 @@ import Joi from 'joi';
 export const createValidationContact = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string().valid('home', 'work', 'personal').required(),
 });
 
 export const updateValidationContact = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-}).or('name', 'phoneNumber');
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string().valid('home', 'work', 'personal').required(),
+});
+// .or('name', 'phoneNumber');
