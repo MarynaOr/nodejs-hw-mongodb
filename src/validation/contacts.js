418a556ec3b29c906d83d5b05314c11b37c7af2f
Maryna <89296066105marina@gmal.com>
2025-07-09 21:39:38 +0200

    fix-req

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,16 +3,15 @@ import Joi from 'joi';
 export const createValidationContact = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  isFavourite: Joi.boolean().required(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('home', 'work', 'personal').required(),
 });
 
 export const updateValidationContact = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().email().required(),
-  isFavourite: Joi.boolean().required(),
-  contactType: Joi.string().valid('home', 'work', 'personal').required(),
-});
-// .or('name', 'phoneNumber');
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('home', 'work', 'personal'),
+}).min(1);
